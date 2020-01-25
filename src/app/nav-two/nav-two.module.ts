e61dacd839927f56d3bf13e5a2de1c6f20d034a0
NabilShahid <nabil110176@gmail.com>
2020-01-25 22:25:06 +0500

    navt secon routing

--- src/app/nav-two/nav-two.module.ts
+++ src/app/nav-two/nav-two.module.ts
@@ -1,24 +1,34 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NavTwoTestCompComponent } from './components/nav-two-test-comp/nav-two-test-comp.component';
-import { NavTwoMainComponent } from './components/nav-two-main/nav-two-main.component';
-import { RouterModule } from '@angular/router';
-import { RegisterFormComponent } from './components/register-form/register-form.component';
-import { RegisterHelpComponent } from './components/register-help/register-help.component';
-import { RegisterConfirmationComponent } from './components/register-confirmation/register-confirmation.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { NavTwoTestCompComponent } from "./components/nav-two-test-comp/nav-two-test-comp.component";
+import { NavTwoMainComponent } from "./components/nav-two-main/nav-two-main.component";
+import { RouterModule } from "@angular/router";
+import { RegisterFormComponent } from "./components/register-form/register-form.component";
+import { RegisterHelpComponent } from "./components/register-help/register-help.component";
+import { RegisterConfirmationComponent } from "./components/register-confirmation/register-confirmation.component";
 
 @NgModule({
-  declarations: [NavTwoTestCompComponent, NavTwoMainComponent, RegisterFormComponent, RegisterHelpComponent, RegisterConfirmationComponent],
+  declarations: [
+    NavTwoTestCompComponent,
+    NavTwoMainComponent,
+    RegisterFormComponent,
+    RegisterHelpComponent,
+    RegisterConfirmationComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       {
-        path: '',
-        component: NavTwoMainComponent
+        path: "",
+        component: NavTwoMainComponent,
+        children: [
+          { path: "register", component: RegisterFormComponent },
+          { path: "register-help", component: RegisterHelpComponent },
+          { path: "register-confirm", component: RegisterConfirmationComponent },
+          { path: "**", redirectTo: "register", pathMatch: "full" }
+        ]
       }
     ])
   ]
 })
-export class NavTwoModule { }
+export class NavTwoModule {}
