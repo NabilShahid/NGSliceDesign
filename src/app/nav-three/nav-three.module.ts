3f6ddaee21ea5e2a74478ea71c33f6efed922692
NabilShahid <nabil110176@gmail.com>
2020-01-27 00:08:35 +0500

    Loading simple implmenetaton ngcontent

--- src/app/nav-three/nav-three.module.ts
+++ src/app/nav-three/nav-three.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavThreeMainComponent } from './components/nav-three-main/nav-three-main.component';
@@ -10,6 +11,7 @@ import { DataListComponent } from './components/data-list/data-list.component';
   declarations: [NavThreeMainComponent, DataListComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild([
       {
         path: '',
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { CurrencyDirective } from './directives/currency.directive';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ThousandSepPipe } from './pipes/thousand-sep.pipe';
+import { LoadingComponent } from './components/loading/loading.component';
 
 
 
 @NgModule({
-  declarations: [UlListComponent,CurrencyDirective, ThousandSepPipe],
+  declarations: [UlListComponent,CurrencyDirective, ThousandSepPipe, LoadingComponent],
   imports: [
     CommonModule,
   ],
-  exports:[UlListComponent,CurrencyDirective,ThousandSepPipe]
+  exports:[UlListComponent,CurrencyDirective,ThousandSepPipe,LoadingComponent]
 })
 export class SharedModule { }
