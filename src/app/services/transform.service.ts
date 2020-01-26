927d215c1376100ce464dc8ccdb099a6b348f54d
NabilShahid <nabil110176@gmail.com>
2020-01-26 16:02:49 +0500

    Register confirm navigation

--- src/app/services/transform.service.ts
+++ src/app/services/transform.service.ts
@@ -5,6 +5,11 @@ import { Injectable } from "@angular/core";
 })
 export class TransformService {
   constructor() {}
+  addThousandSeperator = num =>
+    num.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+  removeNonNumChars = num => num.replace(/\D/g, "");
+
   currencyTransform = num =>
-    "$" + num.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    "$" + this.addThousandSeperator((+this.removeNonNumChars(num)).toString());
 }
